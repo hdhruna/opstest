4600df941836bec9bf3eaf8992045251e2ff307b
Hitesh Narendra Dhruna <dhruna.hitesh@gmail.com>
2019-12-26 18:49:21 +0530

    mods

--- src/main/java/com/example/r42/SuchController.java
+++ src/main/java/com/example/r42/SuchController.java
@@ -9,42 +9,46 @@ import org.springframework.http.ResponseEntity;
 @RestController
 public class SuchController {
 
-    @Value("${suchname}") private String suchName;
-    // @Value("${date_api_host}") private String date_api_host;
-    // @Value("${date_api_port}") private String date_api_port;
-    // 	@RequestMapping("/hello")
-    // 	public String suchHello(){
-    // 		return "hello " + suchName;
-    // 	}
-    // }
-
-    RestTemplate restTemplate = new RestTemplate();
-    String azUrl = "http://169.254.169.254/latest/meta-data/placement/availability-zone";
-    String azName = restTemplate.getForObject(azUrl, String.class);
-    String dateUrl = "http://" + System.getenv("date_api_host") + ":" + System.getenv("date_api_port") + "/date";
-    // ResponseEntity < String > response = restTemplate.getForEntity("http://" + date_api_host + ":" + date_api_port + "/date", String.class);
-    ResponseEntity < String > response = restTemplate.getForEntity(dateUrl, String.class);
-    String date = response.getBody();
-
-    @RequestMapping("/hello")
-    public String suchHello() {
-        return "hello from " + azName + " az, the server local time is " + date;
-    }
-}
-
-
 //     @Value("${suchname}") private String suchName;
+//     // @Value("${date_api_host}") private String date_api_host;
+//     // @Value("${date_api_port}") private String date_api_port;
+//     // 	@RequestMapping("/hello")
+//     // 	public String suchHello(){
+//     // 		return "hello " + suchName;
+//     // 	}
+//     // }
+
+//     RestTemplate restTemplate = new RestTemplate();
+
+//     String azUrl = "http://169.254.169.254/latest/meta-data/placement/availability-zone";
+//     String azName = restTemplate.getForObject(azUrl, String.class);
+    
+//     String dateUrl = "http://" + System.getenv("date_api_host") + ":" + System.getenv("date_api_port") + "/date";
+
+//     // ResponseEntity < String > response = restTemplate.getForEntity("http://" + date_api_host + ":" + date_api_port + "/date", String.class);
+    
+//     ResponseEntity < String > response = restTemplate.getForEntity(dateUrl, String.class);
+//     String date = response.getBody();
 
 //     @RequestMapping("/hello")
-//     public String suchHello(){
-
-//         String azUrl = "http://169.254.169.254/latest/meta-data/placement/availability-zone";
-//         String timeUrl = System.getenv("TIME_URL") + ":" + System.getenv("TIME_PORT");
-//         RestTemplate restTemplate = new RestTemplate();
-
-//         String result = restTemplate.getForObject(timeUrl, String.class);
-//         String azName = restTemplate.getForObject(azUrl, String.class);
-
-//         return "Hello " + azName + " " + result;
+//     public String suchHello() {
+//         return "hello from " + azName + " az, the server local time is " + date;
 //     }
 // }
+
+
+    @Value("${suchname}") private String suchName;
+
+    @RequestMapping("/hello")
+    public String suchHello(){
+
+        String azUrl = "http://169.254.169.254/latest/meta-data/placement/availability-zone";
+        String timeUrl = System.getenv("date_api_host") + ":" + System.getenv("date_api_port");
+        RestTemplate restTemplate = new RestTemplate();
+
+        String result = restTemplate.getForObject(timeUrl, String.class);
+        String azName = restTemplate.getForObject(azUrl, String.class);
+
+        return "Hello from " + azName + " az, the server local time is " + result;
+    }
+}
